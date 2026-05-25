5f23c335164570d7ead9762a7afb3a4a5ae13f00
bryanflores764 <bryanaviles604@gmail.com>
2026-05-25 14:35:03 -0600

    dfde

--- Backend/src/routes/usuarios.routes.js
+++ Backend/src/routes/usuarios.routes.js
@@ -10,10 +10,11 @@ const { verifyToken, verifyAdmin } = require('../middlewares/roles.middleware');
 const { registrarAuditoria } = require('../middlewares/auditoria.middleware');
 
 // ✅ ORDEN CORRECTO
+router.get('/veterinarios',      usuariosController.getVeterinarios);
 router.use(verifyToken);
 router.use(registrarAuditoria('usuarios'));
 
-router.get('/veterinarios',      usuariosController.getVeterinarios);
+
 router.get('/',         verifyAdmin, usuariosController.getAllUsuarios);
 router.post('/',        verifyAdmin, usuariosController.createUsuario);
 router.put('/:id',      verifyAdmin, usuariosController.updateUsuario);
